d3b18c8cc90eb0aa971232324a6626eb5e54ad26
Carl Van Schie <carl.vanschie@gmail.com>
2017-03-14 10:07:57 +0000

    class_generator.py edited online with Bitbucket
    Added in printing of setter booleans
    Added in equalsTo
    Added in hashCode
    Added in toString

--- java_code_generator/class_generator.py
+++ java_code_generator/class_generator.py
@@ -1,37 +1,98 @@
 import json
 
 
-def print_variable(v):
-    print(v)
+def structure_variable(s):
+    return s[0].upper() + s[1:]
 
+def print_variable(v):
     if not isinstance(v, Variable):
         return ""
-    return "    %s %s %s;" % (v.access,v.type, v.name)
+    return "    %s %s %s;" % (v.access, v.type, v.name)
 
 
 def print_getter(v):
     if not isinstance(v, Variable):
         return ""
-    return "    public %s get%s() {\n " \
-           "        return %s; \n" \
-           "    }\n" % (v.type, v.name.title(), v.name)
+
+    return_string = "   public %s" % v.type
+    if v.type == "boolean":
+        return_string = " %s is%s() {\n " % (return_string, structure_variable(v.name))
+    else:
+        return_string = " %s get%s() {\n " % (return_string, structure_variable(v.name))
+
+    return "%s        return this.%s;\n" \
+        "    }\n" % (return_string, v.name)
 
 
 def print_setter(v):
     if not isinstance(v, Variable):
         return ""
     return "    public void set%s(%s %s) {\n " \
-           "        %s = %s; \n" \
-           "    }\n" % (v.name.title(), v.type, v.name,  v.name,  v.name)
+           "        this.%s = %s; \n" \
+           "    }\n" % (structure_variable(v.name), v.type, v.name,  v.name,  v.name)
 
 
 def print_getter_and_setter(v):
-    output = "%s" % (print_getter(v))
+    if not isinstance(v, Variable):
+        return ""
 
+    output = "%s" % (print_getter(v))
     if not v.read_only:
         output = "%s\n%s" % (output, print_setter(v))
 
     return output
+
+
+def print_to_string(c):
+    if not isinstance(c, Class):
+        return ""
+
+    return_string = "" \
+        "    @Override\n" \
+        "    public String toString() {\n" \
+        "        return MoreObjects.toStringHelper(this)\n"
+
+    for v in c.variables:
+        return_string = "%s            .add(\"%s\", %s)\n" % (return_string, v.name, v.name)
+
+    return "%s            .toString();\n" \
+           "    }\n" % return_string
+
+
+def print_equals_to(c):
+    if not isinstance(c, Class):
+        return ""
+
+    return_string = "" \
+        "    @Override\n" \
+        "    public boolean equals(Object o) {\n" \
+        "        if (this == o) return true;\n" \
+        "        if (o == null || getClass() != o.getClass()) return false;\n\n" \
+        "        %s castedObject = (%s) o;\n" % (c.name, c.name)
+
+    # TODO add in variables types here-> Objects.equal(second, testOne.second);
+    for v in c.variables:
+        return_string = "%s" \
+            "        if(%s != castedObject.%s) return false;\n" % (return_string, v.name, v.name)
+
+    return "%s" \
+           "        return true;\n" \
+           "   }\n" % return_string
+
+
+def print_hash_code(c):
+    if not isinstance(c, Class):
+        return ""
+
+    variables = ""
+    for v in c.variables:
+        variables = "%s,%s" % (variables, v.name)
+
+    return "" \
+        "    @Override\n" \
+        "    public int hashCode() {\n" \
+        "        return Objects.hashCode(%s);\n" \
+        "    }\n" % variables[1:]
 
 
 def print_class(c):
@@ -47,10 +108,14 @@ def print_class(c):
     return "public class %s {\n" \
            "" \
            "%s\n" \
-           "%s" \
+           "%s\n" \
            "" \
-           "\n}" \
-        % (c.name, variable_declarations, getter_and_setters)
+           "%s\n" \
+           "%s\n" \
+           "%s\n" \
+           "}" \
+        % (c.name, variable_declarations,
+           getter_and_setters, print_to_string(c), print_equals_to(c), print_hash_code(c))
 
 
 class Variable:
