ca56db69617a343cf55f627cf799bfd1b98a4073
Carl Van Schie <carl.vanschie@gmail.com>
2017-03-14 13:41:42 +0000

    class_generator.py edited online with Bitbucket
    Pylint run and changes made.

--- java_code_generator/class_generator.py
+++ java_code_generator/class_generator.py
@@ -1,50 +1,59 @@
+from __future__ import print_function
 import json
 
+PRIMITIVES = ['long', 'int', 'short', 'byte',
+              'float', 'double', 'boolean', 'char']
 
-def structure_variable(s):
-    return s[0].upper() + s[1:]
 
-def print_variable(v):
-    if not isinstance(v, Variable):
+def uppercase_first_letter(string):
+    return string[0].upper() + string[1:]
+
+
+def print_variable(variable):
+    if not isinstance(variable, Variable):
         return ""
-    return "    %s %s %s;" % (v.access, v.type, v.name)
+    return "    %s %s %s;" % (variable.access, variable.type, variable.name)
 
 
-def print_getter(v):
-    if not isinstance(v, Variable):
+def print_getter(variable):
+    if not isinstance(variable, Variable):
         return ""
 
-    return_string = "   public %s" % v.type
-    if v.type == "boolean":
-        return_string = " %s is%s() {\n " % (return_string, structure_variable(v.name))
+    return_string = "   public %s" % variable.type
+    if variable.type == "boolean":
+        return_string = " %s is%s() {\n " \
+                        % (return_string, uppercase_first_letter(variable.name))
     else:
-        return_string = " %s get%s() {\n " % (return_string, structure_variable(v.name))
+        return_string = " %s get%s() {\n " \
+                        % (return_string, uppercase_first_letter(variable.name))
 
     return "%s        return this.%s;\n" \
-        "    }\n" % (return_string, v.name)
+           "    }\n" % (return_string, variable.name)
 
 
-def print_setter(v):
-    if not isinstance(v, Variable):
+def print_setter(variable):
+    if not isinstance(variable, Variable):
         return ""
     return "    public void set%s(%s %s) {\n " \
            "        this.%s = %s; \n" \
-           "    }\n" % (structure_variable(v.name), v.type, v.name,  v.name,  v.name)
+           "    }\n" \
+           % (uppercase_first_letter(variable.name), variable.type,
+              variable.name, variable.name, variable.name)
 
 
-def print_getter_and_setter(v):
-    if not isinstance(v, Variable):
+def print_getter_and_setter(variable):
+    if not isinstance(variable, Variable):
         return ""
 
-    output = "%s" % (print_getter(v))
-    if not v.read_only:
-        output = "%s\n%s" % (output, print_setter(v))
+    output = "%s" % (print_getter(variable))
+    if not variable.read_only:
+        output = "%s\n%s" % (output, print_setter(variable))
 
     return output
 
 
-def print_to_string(c):
-    if not isinstance(c, Class):
+def print_to_string(current_class):
+    if not isinstance(current_class, Class):
         return ""
 
     return_string = "" \
@@ -52,58 +61,73 @@ def print_to_string(c):
         "    public String toString() {\n" \
         "        return MoreObjects.toStringHelper(this)\n"
 
-    for v in c.variables:
-        return_string = "%s            .add(\"%s\", %s)\n" % (return_string, v.name, v.name)
+    for variable in current_class.variables:
+        return_string = "%s" \
+            "            .add(\"%s\", %s)\n" \
+                        % (return_string, variable.name, variable.name)
 
     return "%s            .toString();\n" \
            "    }\n" % return_string
 
 
-def print_equals_to(c):
-    if not isinstance(c, Class):
+def print_equals_to(current_class):
+    if not isinstance(current_class, Class):
         return ""
 
     return_string = "" \
         "    @Override\n" \
         "    public boolean equals(Object o) {\n" \
         "        if (this == o) return true;\n" \
-        "        if (o == null || getClass() != o.getClass()) return false;\n\n" \
-        "        %s castedObject = (%s) o;\n" % (c.name, c.name)
+        "        if (o == null || getClass() != o.getClass()) " \
+                    "return false;\n\n" \
+        "        %s castedObject = (%s) o;\n" \
+                    % (current_class.name, current_class.name)
 
-    # TODO add in variables types here-> Objects.equal(second, testOne.second);
-    for v in c.variables:
-        return_string = "%s" \
-            "        if(%s != castedObject.%s) return false;\n" % (return_string, v.name, v.name)
+    for variable in current_class.variables:
+        if variable.type in PRIMITIVES:
+            return_string = "%s" \
+                            "        if(%s != castedObject.%s) " \
+                            "return false;\n" \
+                            % (return_string, variable.name, variable.name)
+        else:
+            return_string = "%s" \
+                            "        if(!Objects.equal(%s, castedObject.%s)) " \
+                            "return false;\n" \
+                            % (return_string, variable.name, variable.name)
 
     return "%s" \
            "        return true;\n" \
            "   }\n" % return_string
 
 
-def print_hash_code(c):
-    if not isinstance(c, Class):
+def print_hash_code(current_class):
+    if not isinstance(current_class, Class):
         return ""
 
     variables = ""
-    for v in c.variables:
-        variables = "%s,%s" % (variables, v.name)
+    for variable in current_class.variables:
+        variables = "%s,%s" % (variables, variable.name)
 
     return "" \
-        "    @Override\n" \
-        "    public int hashCode() {\n" \
-        "        return Objects.hashCode(%s);\n" \
-        "    }\n" % variables[1:]
+           "    @Override\n" \
+           "    public int hashCode() {\n" \
+           "        return Objects.hashCode(%s);\n" \
+           "    }\n" % variables[1:]
 
 
-def print_class(c):
-    if not isinstance(c, Class):
+def print_class(current_class):
+    if not isinstance(current_class, Class):
         return ""
 
     variable_declarations = ""
     getter_and_setters = ""
-    for v in c.variables:
-        variable_declarations = "%s\n%s" % (variable_declarations, print_variable(v))
-        getter_and_setters = "%s\n%s" % (getter_and_setters, print_getter_and_setter(v))
+    for variable in current_class.variables:
+        variable_declarations = "%s\n%s" \
+                                % (variable_declarations,
+                                   print_variable(variable))
+        getter_and_setters = "%s\n%s" \
+                             % (getter_and_setters,
+                                print_getter_and_setter(variable))
 
     return "public class %s {\n" \
            "" \
@@ -114,16 +138,22 @@ def print_class(c):
            "%s\n" \
            "%s\n" \
            "}" \
-        % (c.name, variable_declarations,
-           getter_and_setters, print_to_string(c), print_equals_to(c), print_hash_code(c))
+           % (current_class.name, variable_declarations, getter_and_setters,
+              print_to_string(current_class), print_equals_to(current_class),
+              print_hash_code(current_class))
 
 
-class Variable:
-    def __init__(self, json):
-        self.name = json["name"]
-        self.type = json["type"]
-        self.access = "private" if "access" not in json else json["access"]
-        self.read_only = False if "read_only" not in json or json["read_only"] == "false" else True
+class Variable(object):
+    def __init__(self, input_json):
+        self.name = input_json["name"]
+        self.type = input_json["type"]
+        self.access = "private" \
+            if "access" not in input_json \
+            else input_json["access"]
+        self.read_only = False \
+            if "read_only" not in input_json \
+               or input_json["read_only"] == "false" \
+            else True
 
     def __repr__(self):
         return "%s(name=%r, type=%r, access=%r, readOnly=%r)" \
@@ -133,14 +163,15 @@ class Variable:
                   self.access,
                   self.read_only)
 
-class Class:
-    def __init__(self, json):
-        self.name = json['name']
+
+class Class(object):
+    def __init__(self, input_json):
+        self.name = input_json['name']
         self.variables = []
 
-        if 'variables' in json:
-            for v in json["variables"]:
-                self.variables.append(Variable(v))
+        if 'variables' in input_json:
+            for variable_json in input_json["variables"]:
+                self.variables.append(Variable(variable_json))
 
     def __repr__(self):
         return "%s(name=%r, variables=%s)" \
@@ -154,11 +185,11 @@ def main():
         data = json.load(data_file)
 
     classes = []
-    for c in data["classes"]:
-        classes.append(Class(c))
+    for class_json in data["classes"]:
+        classes.append(Class(class_json))
 
-    for c in classes:
-        print(print_class(c))
+    for class_json in classes:
+        print(print_class(class_json))
 
 
 if __name__ == "__main__":
